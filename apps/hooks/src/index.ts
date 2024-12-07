707007b578b67b3a31338feeea3198980369da6a
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-12-07 22:56:30 +0530

    basic database structure, test api endpoint for metadata

--- apps/hooks/src/index.ts
+++ apps/hooks/src/index.ts
@@ -1,1 +1,34 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
+
+
+const client = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    await client.$transaction(async tx =>{
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        })
+
+        await client.zapRunOutBox.create({
+            data: {
+                zapRunId: run.id,
+            }
+        })
+    })
+    res.json({ message: "webhook received" });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
