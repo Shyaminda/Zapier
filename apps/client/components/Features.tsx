5d8b03a9433164192bac5e19f851b4c782924177
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-12-11 09:38:09 +0530

    add login page

--- apps/client/components/Features.tsx
+++ apps/client/components/Features.tsx
@@ -19,7 +19,7 @@ export const Features = ({title, subtitle}: {
 }
 
 function Check() {
-    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
     </svg>
 }
--- packages/ui/src/primary-button.tsx
+++ packages/ui/src/primary-button.tsx
@@ -4,7 +4,7 @@ export const PrimaryButton = ({children, onClick, size = "small"}: {
   onClick: () => void;
   size?: "small" | "large";
 }) => {
-  return <div onClick={onClick} className={`bg-amber-700 text-white text-center rounded-full hover:shadow-md cursor-pointer ${size === "small" ? "text-sm" : "text-lg"} ${size === "small" ? "px-8 pt-" : "px-10  py-4"} `}>
+  return <div onClick={onClick} className={`bg-amber-700 text-white text-center rounded-full items-center hover:shadow-md cursor-pointer ${size === "small" ? "text-sm" : "text-lg"} ${size === "small" ? "px-8 pt-1.5" : "px-10  py-4"} `}>
     {children}
   </div>
 }
